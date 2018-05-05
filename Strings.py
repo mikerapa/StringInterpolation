9e72d98fb328edf3656fe839f623c977387d2b17
Mike Rapa <Mike@mikerapa.com>
2018-05-04 22:29:09 -0400

    Initial commit

---
+++ Strings.py
@@ -0,0 +1,8 @@
+direction = "Left"
+distance = 2
+
+# just strings
+print(f"Turn {direction} in {distance}km.")
+
+# with mathmatical expression
+print(f"Turn {direction} in {distance * .621} miles.")
